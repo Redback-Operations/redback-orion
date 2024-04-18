aa419f80266cb48eee73b748d127ad0a0fa52797
Mayank52455 <sharmamayank16760@gmail.com>
2024-04-18 20:28:06 +1000

    Fixed "path not found: SerialPort" error
    SerialPort v9.4 requires the path to be defined in parameters.

--- dataTransfer/index.js
+++ dataTransfer/index.js
@@ -1,10 +1,11 @@
 const { SerialPort } = require('serialport');
 const { ReadlineParser } = require('@serialport/parser-readline');
 const fs = require('fs');
+const path = require('path'); // Require the 'path' module
 
 // Set the correct COM port name and CSV file path
 const portName = 'COM3';
-const csvFilePath = '\data transfer\mayank_ll.csv';
+const csvFilePath = path.join(__dirname, 'mayank_ll.csv'); // Use path.join to create the file path
 
 // Create a writable stream to the CSV file
 const csvStream = fs.createWriteStream(csvFilePath);
@@ -13,7 +14,7 @@ const csvStream = fs.createWriteStream(csvFilePath);
 csvStream.write('time,data values\n');
 
 // Create a SerialPort instance
-const port = new SerialPort(portName, { baudRate: 9600 });
+const port = new SerialPort({ path: portName, baudRate: 9600 });
 
 // Create a Readline parser instance
 const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }));
