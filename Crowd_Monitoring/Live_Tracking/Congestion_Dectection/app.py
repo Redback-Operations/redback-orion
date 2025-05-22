819d03d9572d91252424c3354ed6ac70c9fa3356
shricenguyen <amborse31@gmail.com>
2025-05-22 21:34:26 +1000

    Editing app.py for security reason

--- Crowd_Monitoring/Live_Tracking/Congestion_Dectection/app.py
+++ Crowd_Monitoring/Live_Tracking/Congestion_Dectection/app.py
@@ -8,24 +8,31 @@ cors = CORS(app)
 cameraProcessor = CameraProcessor()
 db = Database()
 
+
 # routes for people count
 @app.route("/api/peopleCount", methods=["GET"])
 def getPeopleCount():
     count = db.getlastestRecord()
     return jsonify({"peopleCount": count})
 
+
 # routes for main camera display
 @app.route("/LiveTracking/videoFeed", methods=["GET"])
 def videoFeed():
-    return Response(stream_with_context(cameraProcessor.getFrame()),
-                    mimetype="multipart/x-mixed-replace; boundary=frame")
+    return Response(
+        stream_with_context(cameraProcessor.getFrame()),
+        mimetype="multipart/x-mixed-replace; boundary=frame",
+    )
+
 
 # routes for annotated frame or 2d floor plan
 @app.route("/LiveTracking/annotatedVideoFeed", methods=["GET"])
 def annotatedVideoFeed():
-    return Response(stream_with_context(cameraProcessor.getAnnotatedFrame()),
-                    mimetype="multipart/x-mixed-replace; boundary=frame")
+    return Response(
+        stream_with_context(cameraProcessor.getAnnotatedFrame()),
+        mimetype="multipart/x-mixed-replace; boundary=frame",
+    )
 
-    
+
 if __name__ == "__main__":
-    app.run(debug=True, port=8000)
+    app.run(port=8000)
