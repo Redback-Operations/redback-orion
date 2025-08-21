af9e7628ef10e48db2c1bbe2b89bab007de78afa
ishita230702 <167290181+ishita230702@users.noreply.github.com>
2025-08-21 03:27:13 +0000

    Add AFL Dashboard route to App.tsx
    cgen-cf16335ff11e49dd8fe93c26d1d64c65

--- client/App.tsx
+++ client/App.tsx
@@ -7,6 +7,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
+import AFLDashboard from "./pages/AFLDashboard";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
