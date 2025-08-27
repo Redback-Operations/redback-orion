da577c3970c54ab6145c2a9b1b30bcbe27986425
ishita230702 <167290181+ishita230702@users.noreply.github.com>
2025-08-27 09:33:48 +0000

    Fix App.tsx to only export App component, remove createRoot call
    cgen-e0b1b5cce8f4435380fbeb804bc042ef

--- client/App.tsx
+++ client/App.tsx
@@ -1,7 +1,4 @@
-import "./global.css";
-
 import { Toaster } from "@/components/ui/toaster";
-import { createRoot } from "react-dom/client";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
